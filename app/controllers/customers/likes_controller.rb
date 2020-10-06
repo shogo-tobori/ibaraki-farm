ba9565dbfced4270fc02606632bc87188a929bc1
Shogo Tobori <qb.brady.121212@gmail.com>
2020-10-06 16:38:49 +0000

    [add]いいね機能実装完了

--- app/controllers/customers/likes_controller.rb
+++ app/controllers/customers/likes_controller.rb
@@ -1,14 +1,14 @@
 class Customers::LikesController < ApplicationController
 
   def create
-    @like = Like.new(customer_id: @current_customer.id, item_id: params[:item_id])
+    @like = Like.new(customer_id: current_customer.id, item_id: params[:item_id])
     @like.save
-    redirect_to("/items/#{params[:tweet_id]}")
+    redirect_to("/customers/items/#{params[:item_id]}")
   end
 
   def destroy
-    @like = Like.find_by(customer_id: @current_customer.id, item_id: params[item_id])
+    @like = Like.find_by(customer_id: current_customer.id, item_id: params[:item_id])
     @like.destroy
-    redirect_to("/items/#{params[:item_id]}")
+    redirect_to("/customers/items/#{params[:item_id]}")
   end
 end
