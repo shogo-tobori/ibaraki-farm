9471a3fe85efd00228de150e9c91be6696e202a7
Shogo Tobori <qb.brady.121212@gmail.com>
2020-10-11 12:00:16 +0000

    [delete]seedの中身

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,3 @@ Admin.create!(
   password: '00000000',
   password_confirmation: '00000000',
 )
-
-Customer.create!(
-	first_name: 'hoge',
-	last_name: '1',
-	first_name_kana: 'hoge',
-	last_name_kana: '1',
-	postal_code: '1111111',
-	address: '群馬県',
-	email: 'hoge1@hoge1',
-	password: '11111111',
-	password_confirmation: '11111111',
-	phone: '11111111111'
-)
