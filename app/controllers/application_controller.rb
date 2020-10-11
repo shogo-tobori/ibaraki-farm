19d6a1bcc9bbee6264fdd241c19aae73da863076
Shogo Tobori <qb.brady.121212@gmail.com>
2020-10-11 11:03:03 +0000

    [fix]is_deletedカラム修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   	def configure_permitted_parameters
-    	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone])
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :phone, :is_deleted])
   	end
 
   end
