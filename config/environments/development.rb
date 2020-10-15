87d7fee1f1e62658bbc7533020acd810ce39f3f5
Shogo Tobori <qb.brady.121212@gmail.com>
2020-10-15 06:02:24 +0000

    [fix]メール送信を本番環境で行う為にproduction.rbに記述を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # 本番環境用（メール送信）
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:             ENV['SEND_MAIL'],
+  password:              ENV['SEND_MAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -92,16 +105,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                  587,
-  domain:               'gmail.com',
-  user_name:             ENV['SEND_MAIL'],
-  password:              ENV['SEND_MAIL_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto:  true
-}
 end
