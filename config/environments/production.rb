5e555eace2b933f6f874efbf370be86e820a84f9
Shogo Tobori <qb.brady.121212@gmail.com>
2020-10-15 06:40:05 +0000

    [fix]メール送信を本番環境で行う為にproduction.rbに記述を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   port:                  587,
   domain:               'gmail.com',
   user_name:             ENV['SEND_MAIL'],
-  password:              ENV['SEND_MAIL_PASSWORD_PRPDUCTION'],
+  password:              "pxjszfzzjmlidwgj",
   authentication:       'plain',
   enable_starttls_auto:  true
 }
