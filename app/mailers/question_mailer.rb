ee399d558ffa4e0dd9cceb78502a8c3b08517342
Shogo Tobori <qb.brady.121212@gmail.com>
2020-11-05 15:27:41 +0000

    [add]mailers/question_mailer.rb 環境変数化

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,9 +1,9 @@
 class QuestionMailer < ApplicationMailer
-  default from: "qb.brady.121212@gmail.com"   # 送信元アドレス
+  default from: ENV['SEND_MAIL']   # 送信元アドレス
 
   def received_email(question)
     @question = question
     # 送信先のメールアドレスを記述
-    mail(:to => "qb.brady.121212@gmail.com", :subject => 'お問い合わせを承りました')
+    mail(:to => ENV['SEND_MAIL'], :subject => 'お問い合わせを承りました')
   end
 end
