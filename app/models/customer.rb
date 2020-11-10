cf6506c5bc18ef046ffa7004609a2c71a7c99e66
Shogo Tobori <qb.brady.121212@gmail.com>
2020-11-10 08:27:53 +0000

    [fix]device・customerモデルのエラーメッセージの日本語化

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,6 +14,7 @@ class Customer < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :phone, presence: true
+  validates :password_confirmation, presence: true
 
   enum is_deleted: { 有効: false, 退会済み: true }
 
